ab72f167ecb29f485a01e89e3c5ca9065c0102ae
joemc9011 <joemc9011@gmail.com>
2021-01-17 22:44:34 -0500

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true });
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 require("./routes/api.js")(app);
 require("./routes/html.js")(app);
 
